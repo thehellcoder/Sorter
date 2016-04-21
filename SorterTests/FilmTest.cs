ebb111aa8bba044484c39c546fa07f1a5480fe5a
thehellcoder <root@thehellcoder>
2016-04-21 09:16:15 +0400

    Test: Film.getRowsCount()

---
+++ SorterTests/FilmTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sorter;
+
+namespace SorterTests
+{
+    [TestClass]
+    public class FilmTest
+    {
+        [TestMethod]
+        public void TestGetRowsCount()
+        {
+            Film tmp = new Film("Экипаж", "trailer.mp4", "3:00", "6+");
+            for(int i = 1; i <= 9; i++)
+            {
+                tmp.AddSession("10:00", "100500", "IMAX");
+            }
+            Assert.AreEqual(1, tmp.getRowsCount());
+        }
+    }
+}

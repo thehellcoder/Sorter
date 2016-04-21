e8d762f29ec709ecec72fd03056553de624ee193
thehellcoder <root@thehellcoder>
2016-04-21 11:46:50 +0400

    FilmTest - cosmetic changes

--- SorterTests/FilmTest.cs
+++ SorterTests/FilmTest.cs
@@ -10,12 +10,31 @@ namespace SorterTests
         [TestMethod]
         public void TestGetRowsCount()
         {
-            Film tmp = new Film("Экипаж", "trailer.mp4", "3:00", "6+");
-            for(int i = 1; i <= 9; i++)
+            Film f1 = new Film("Экипаж", "trailer.mp4", "3:00", "6+");
+            f1.AddSession("10:00", "100500", "IMAX");
+
+            Film f2 = new Film("Экипаж", "trailer.mp4", "3:00", "6+");
+            for (int i = 1; i <= 9; i++)
             {
-                tmp.AddSession("10:00", "100500", "IMAX");
+                f2.AddSession("10:00", "100500", "IMAX");
             }
-            Assert.AreEqual(1, tmp.getRowsCount());
+
+            Film f3 = new Film("Экипаж", "trailer.mp4", "3:00", "6+");
+            for (int i = 1; i <= 10; i++)
+            {
+                f3.AddSession("10:00", "100500", "IMAX");
+            }
+
+            Film f4 = new Film("Экипаж", "trailer.mp4", "3:00", "6+");
+            for (int i = 1; i <= 18; i++)
+            {
+                f4.AddSession("10:00", "100500", "IMAX");
+            }
+
+            Assert.AreEqual(1, f1.getRowsCount());
+            Assert.AreEqual(1, f2.getRowsCount());
+            Assert.AreEqual(2, f3.getRowsCount());
+            Assert.AreEqual(2, f4.getRowsCount());
         }
     }
 }
